0e65a4abdf364d1ef48d02394511fc154122e2cb
Anton Burmagin <burmagin.anton.s@gmail.com>
2025-03-06 02:04:07 +0300

    8th commit: some clean

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -3,14 +3,11 @@ package pages;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import waiter.CustomWaiter;
 
-import java.time.Duration;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
--- src/main/java/waiter/CustomWaiter.java
+++ src/main/java/waiter/CustomWaiter.java
@@ -1,12 +1,10 @@
 package waiter;
 
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import pages.MainPage;
 
 import java.time.Duration;
 
